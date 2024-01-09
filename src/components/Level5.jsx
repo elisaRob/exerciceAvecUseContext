fe9fb76981e731181a3570d4b851a25b154eb506
Elisabeth ROBERT AOUCHER <elisabethrobertaoucher@gmail.com>
2024-01-09 09:36:55 +0100

    ajouts

--- src/components/Level5.jsx
+++ src/components/Level5.jsx
@@ -1,8 +1,15 @@
+import { ThemeModeContext } from "../contexts/ThemeModeContext";
+import { useContext } from "react"
+
 export function Level5(props) {
-  return (
+  const { themeMode, setThemeMode } = useContext(ThemeModeContext)
+  function toggleThemeMode(){
+    setThemeMode(themeMode==="light" ? "dark" : "light")
+  }
+    return (
     <>
       <div>Niveau de profondeur 5</div>
-      <button>Changer le mode du theme </button>
+      <button onClick={toggleThemeMode}>Changer le mode du theme {themeMode === "light"? "dark" : "light" }</button>
     </>
   );
 }
